ba8245f28fc9e45339b03d6faa8a60a0eb0d676e
Yi1lan <yanli.wang22@imperial.ac.uk>
2026-08-19 13:37:54 +0800

    feat: Lecture 1 (finished); Lecture 2 (till page 7)

--- Tutorial/ProblemSet0/ProblemSet0.py
+++ Tutorial/ProblemSet0/ProblemSet0.py
@@ -7,11 +7,11 @@
 # Answer 2: 
 
 
-#Question 3: 23.0 to the 5th power
-#Answer 3: 
+# Question 3: 23.0 to the 5th power
+# Answer 3: 
 
-#Question 4: Positive root of the following equation: a. 34*x^2 + 68*x - 510 
-#Answer 4: 
+# Question 4: Positive root of the following equation: a. 34*x^2 + 68*x - 510 
+# Answer 4: 
 
 #Question 5: .cos(3.4)**2+math.sin(3.4)**2 
-#Answer 5: 
+# Answer 5: 
